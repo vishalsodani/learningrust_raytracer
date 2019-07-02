a12542ef24350aab2059cf908a5b25179f805a84
Vishal Sodani <vishalsodani@rediffmail.com>
2019-07-02 20:16:41 +0530

    formatted code using rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,12 @@ pub mod raytracer {
     pub struct Vec3 {
         pub e1: f32,
         pub e2: f32,
-        pub e3: f32
+        pub e3: f32,
     }
 
     impl Vec3 {
         fn length(self) -> f32 {
-            (self.e1*self.e1 + self.e2*self.e2 + self.e3*self.e3).sqrt()
+            (self.e1 * self.e1 + self.e2 * self.e2 + self.e3 * self.e3).sqrt()
         }
 
         fn y(self) -> f32 {
@@ -26,7 +26,7 @@ pub mod raytracer {
             let e2: f32 = self.e2 + _rhs.e2;
             let e3: f32 = self.e3 + _rhs.e3;
 
-            Vec3 { e1, e2, e3}
+            Vec3 { e1, e2, e3 }
         }
     }
 
@@ -34,21 +34,29 @@ pub mod raytracer {
         type Output = Vec3;
 
         fn div(self, t: f32) -> Vec3 {
-            Vec3 { e1: self.e1/t, e2: self.e2/t, e3: self.e3/t}
+            Vec3 {
+                e1: self.e1 / t,
+                e2: self.e2 / t,
+                e3: self.e3 / t,
+            }
         }
     }
-    
+
     impl ops::Mul<f32> for Vec3 {
         type Output = Vec3;
 
         fn mul(self, t: f32) -> Vec3 {
-            Vec3 {e1: t* self.e1, e2: t*self.e2, e3: t*self.e3}
+            Vec3 {
+                e1: t * self.e1,
+                e2: t * self.e2,
+                e3: t * self.e3,
+            }
         }
     }
 
     pub struct Ray {
         pub A: Vec3,
-        pub B: Vec3
+        pub B: Vec3,
     }
 
     impl Ray {
@@ -60,17 +68,26 @@ pub mod raytracer {
             self.B
         }
 
-        pub fn point_at_parameter(self, t :f32) -> Vec3 {
-            self.A + (self.B*t)
+        pub fn point_at_parameter(self, t: f32) -> Vec3 {
+            self.A + (self.B * t)
         }
 
         pub fn color(self) -> Vec3 {
             let unit_direction: Vec3 = self.direction();
             let length = unit_direction.length();
 
-            let unit_direction :Vec3 = unit_direction / length;
-            let t: f32 = (unit_direction.y()  + 1.0)*0.5;
-            Vec3{e1:1.0, e2:1.0, e3: 1.0}*(1.0-t) + Vec3{e1:0.5, e2:0.7, e3:1.0}*t
+            let unit_direction: Vec3 = unit_direction / length;
+            let t: f32 = (unit_direction.y() + 1.0) * 0.5;
+            Vec3 {
+                e1: 1.0,
+                e2: 1.0,
+                e3: 1.0,
+            } * (1.0 - t)
+                + Vec3 {
+                    e1: 0.5,
+                    e2: 0.7,
+                    e3: 1.0,
+                } * t
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::fs::File;
-use std::path::Path;
-use std::io::Write;
-use hello_graphics::raytracer::Vec3;
 use hello_graphics::raytracer::Ray;
+use hello_graphics::raytracer::Vec3;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 
 fn main() -> Result<(), std::io::Error> {
     let nx = 200;
@@ -15,10 +15,26 @@ fn main() -> Result<(), std::io::Error> {
     writeln!(file, "{} {}", nx, ny)?;
     writeln!(file, "{}", 255)?;
 
-    let lower_left_corner: Vec3 = Vec3 { e1:-2.0, e2:-1.0, e3:-1.0};
-    let horizontal: Vec3 = Vec3 { e1:4.0, e2:0.0, e3:0.0};
-    let vertical: Vec3 = Vec3 { e1: 0.0, e2: 2.0, e3: 0.0};
-    let origin: Vec3 = Vec3 { e1: 0.0, e2: 0.0, e3: 0.0};
+    let lower_left_corner: Vec3 = Vec3 {
+        e1: -2.0,
+        e2: -1.0,
+        e3: -1.0,
+    };
+    let horizontal: Vec3 = Vec3 {
+        e1: 4.0,
+        e2: 0.0,
+        e3: 0.0,
+    };
+    let vertical: Vec3 = Vec3 {
+        e1: 0.0,
+        e2: 2.0,
+        e3: 0.0,
+    };
+    let origin: Vec3 = Vec3 {
+        e1: 0.0,
+        e2: 0.0,
+        e3: 0.0,
+    };
 
     let j = ny - 1;
     let i = nx;
@@ -27,7 +43,10 @@ fn main() -> Result<(), std::io::Error> {
         for m in 0..i {
             let u: f32 = m as f32 / nx as f32;
             let v: f32 = n as f32 / ny as f32;
-            let r: Ray = Ray { A: origin, B: lower_left_corner + (horizontal*u) + (vertical*v) };
+            let r: Ray = Ray {
+                A: origin,
+                B: lower_left_corner + (horizontal * u) + (vertical * v),
+            };
             let col: Vec3 = r.color();
             let ir: i32 = (255.99 * col.e1) as i32;
             let ig: i32 = (255.99 * col.e2) as i32;
